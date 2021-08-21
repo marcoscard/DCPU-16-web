2985dd8deaeaca8509ed45b32bbbc030f7833231
Marcos Antonio <marcoscardoso.programmer@gmail.com>
2021-08-20 23:51:52 -0300

    add source code and specs

---
+++ src/Emulator.js
@@ -0,0 +1,35 @@
+
+const register_names = ["A","B","C","X","Y","Z","I","J"]
+
+class Emulator {
+    constructor() {
+        this.ramSize = 0x10000
+        this.registers = []
+    }
+    
+    run() {
+        for(var i=0; i<Infinity; i++){
+            decodeBinary()
+            
+        }
+    }
+
+    decodeBinary() {
+       countCycle()
+
+    }
+
+    execute() {
+
+    }
+
+    countCycle() {
+
+    }
+
+}
+
+function main() {
+    emulator = new Emulator()    
+    emulator.run()
+}

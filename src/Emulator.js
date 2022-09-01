75c9b688379f880d5681b4e9dc44c88754ef6d30
Wookyz <wookyz@pop-os.localdomain>
2022-09-01 13:48:43 -0300

    make addresFor function

--- src/Emulator.js
+++ src/Emulator.js
@@ -6,37 +6,41 @@ class Emulator {
         this.ramSize = 0x10000
         this.memory = []
         this.memory.length = this.ramSize
-        this.registers = ["A","B","C","X","Y","Z","I","J"]
+        this.registers = ["A", "B", "C", "X", "Y", "Z", "I", "J"]
+        this.cycle = 0
+        this.memory.sp = 0
+        this.memory.ex = 0
     }
 
     run() {
-        for (var i = 0; i < Infinity; i++) {
-            countCycle()
-            var instruction = this.fetchInstruction()
-            var decodedInstruction = this.decode(instruction)
+        for (let i = 0; i < Infinity; i++) {
+
+            let instruction = this.fetchInstruction()
+            let decodedInstruction = this.decode(instruction)
             //CHAIN METHODS
             this.execute(decodedInstruction)
         }
     }
 
     fetchInstruction() {
-        var pc = this.memory.pc
-        var instruction = this.memory[pc++]
+        let pc = this.memory.pc
+        let instruction = this.memory[pc++]
+        this.cycle++
         return instruction
     }
 
     decode(binary) {
         if ((binary & 0x1f) === 0) {
-            var instruction = this.getSpecialInstruction(binary)
+            let instruction = this.getSpecialInstruction(binary)
         } else {
-            var instruction = this.getInstruction(binary)
+            let instruction = this.getInstruction(binary)
         }
         return instruction
     }
 
     execute(decodedBinary) {
-        var a = this.get(decodedBinary.a)
-        var b = this.get(decodedBinary.b)
+        let a = this.get(decodedBinary.a)
+        let b = this.get(decodedBinary.b)
 
         switch (instruction.opcode) {
             case OPCODES.SET:
@@ -46,10 +50,6 @@ class Emulator {
             default:
                 break;
         }
-    }
-
-    countCycle(times = 1) {
-
     }
 
     getSpecialInstruction(binary) {
@@ -65,50 +65,64 @@ class Emulator {
             opcode: (binary & 0x1f),
             a: this.addressFor({ value: (binary >> 10) & 0x3f, isA: true }),
             b: this.addressFor({ value: (binary >> 5) & 0x1f, isA: false })
-            // TODO addressfor
         }
     }
 
-    addressFor({ value,isA}) {
-        var register, address
-        if( value <= 0x7){
-            register = this.registers[value]
+    addressFor({ value, isA }) {
+        if (value <= 0x7) {
+            let register = this.registers[value]
+            return register
         }
-        else if ( value <= 0x0f){
-            address = this.memory[value % 0x08]
+        else if (value <= 0x0f) {
+            let binary = this.memory[value % 8]
+            return binary
         }
-        else if ( value <= 0x17){
-            var nextInstruction = this.memory[this.memory.pc++]
-            address = (nextInstruction + this.memory[register])
+        else if (value <= 0x17) {
+            let nextInstruction = this.fetchInstruction()
+            let binary = (nextInstruction + this.memory[register])
+            return binary
         }
-        else if ( value === 0x19) {
-            var sp = this.memory.sp
-            return this.memory[sp]
-        } 
-        else if ( value === 0x1a) {
-            var nextInstruction = this.memory[this.memory.pc++]
-            var sp = this.memory.sp
-            return address = (nextInstruction + this.memory[nextInstruction + sp])
-        } 
-        else if ( value === 0x1b) {
-            return this.memory.sp
+        else if (value >= 0x20 && value <= 0x3f) {
+            let literal = value - 0x21
+            return literal
         }
-        else if ( value === 0x1c) {
-            return this.memory.pc
+        switch (value) {
+            case 0x18:
+                if (isA) {
+                    sp = this.memory.sp++
+                    return this.memory[sp]
+                }
+                else {
+                    sp = this.memory.sp - 1
+                    return this.memory[sp]
+                }
+
+            case 0x19:
+                return this.memory[this.memory.sp]
+
+            case 0x1a:
+                var nextInstruction = this.memory[this.memory.pc++]
+                let sp = this.memory.sp
+                return address = this.memory[sp + nextInstruction]
+
+            case 0x1b:
+                return this.memory.sp
+
+            case 0x1c:
+                return this.memory.pc
+
+            case 0x1d:
+                return this.memory.ex
+
+            case 0x1e:
+                var nextInstruction = this.fetchInstruction()
+                return this.memory[nextInstruction]
+
+            case 0x1f:
+                var nextInstruction = this.fetchInstruction()
+                return nextInstruction
         }
-        else if ( value === 0x1d ) {
-            return this.memory.ex
-        }
-        else if ( value === 0x1e) {
-            return this.memory[this.memory.pc++]
-        }
-        else if ( value === 0x1f) {
-            //TODO
-        }
-        else if ( value === 0x18) {
-            //TODO
-        }
-        
+
     }
 
 

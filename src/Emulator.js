1167368128f423d553c69f476d7313b897304da2
BuildTools <unconfigured@null.spigotmc.org>
2023-02-19 22:24:44 -0300

    Changes in execute method

--- src/Emulator.js
+++ src/Emulator.js
@@ -6,7 +6,7 @@ class Emulator {
         this.ramSize = 0x10000
         this.memory = []
         this.memory.length = this.ramSize
-        this.registers = ["A","B","C","X","Y","Z","I","J"]
+        this.registers = ["A", "B", "C", "X", "Y", "Z", "I", "J"]
     }
 
     run() {
@@ -14,7 +14,6 @@ class Emulator {
             countCycle()
             var instruction = this.fetchInstruction()
             var decodedInstruction = this.decode(instruction)
-            //CHAIN METHODS
             this.execute(decodedInstruction)
         }
     }
@@ -26,6 +25,7 @@ class Emulator {
     }
 
     decode(binary) {
+        var instruction
         if ((binary & 0x1f) === 0) {
             var instruction = this.getSpecialInstruction(binary)
         } else {
@@ -35,28 +35,151 @@ class Emulator {
     }
 
     execute(decodedBinary) {
-        var a = this.get(decodedBinary.a)
-        var b = this.get(decodedBinary.b)
+        var a = decodedBinary.a
+        var b = decodedBinary.b
 
         switch (instruction.opcode) {
             case OPCODES.SET:
-                instruction.a = instruction.b
+                this.set(b, this.get(a))
+            case OPCODES.ADD:
+                this.set(b, this.get(b) + this.get(a))
+                if (this.get(b) > 0xffff) {
+                    this.memory.ex = 0x0001
+                }
                 break;
-
+            case OPCODES.SUB:
+                this.set(b, this.get(b) - this.get(a))
+                if (this.get(b) < 0x0000) {
+                    this.memory.ex = 0xffff
+                }
+                break;
+            case OPCODES.MUL:
+                this.set(b, this.get(b) * this.get(a))
+                this.memory.ex = (this.get(b) >> 16) & 0xffff
+                break;
+            case OPCODES.DIV:
+                if (this.get(a) === 0) {
+                    this.set(b, 0)
+                    this.memory.ex = 0
+                } else {
+                    this.set(b, this.get(b) / this.get(a))
+                    this.memory.ex = ((this.get(b) << 16) / this.get(a)) & 0xffff
+                }
+                break;
+            case OPCODES.MOD:
+                if (this.get(a) === 0) {
+                    this.set(b, 0)
+                } else {
+                    this.set(b, this.get(b) % this.get(a))
+                }
+                break;
+            case OPCODES.AND:
+                this.set(b, this.get(b) & this.get(a))
+                break;
+            case OPCODES.BOR:
+                this.set(b, this.get(b) | this.get(a))
+                break;
+            case OPCODES.XOR:
+                this.set(b, this.get(b) ^ this.get(a))
+                break;
+            case OPCODES.SHR:
+                this.memory.ex = ((this.get(b) << 16) >> this.get(a)) & 0xffff
+                this.set(b, this.get(b) >>> this.get(a))
+                break;
+            case OPCODES.ASR:
+                this.memory.ex = ((this.get(b) << 16) >>> this.get(a)) & 0xffff
+                this.set(b, this.get(b) >> this.get(a))
+                break;
+            case OPCODES.SHL:
+                this.memory.ex = ((this.get(b) << this.get(a)) >> 16) & 0xffff
+                this.set(b, this.get(b) << this.get(a))
+                break;
+            case OPCODES.IFB:
+                if ((this.get(b) & this.get(a)) !== 0) {
+                    this.memory.pc++
+                }
+                break;
+            case OPCODES.IFC:
+                if ((this.get(b) & this.get(a)) === 0) {
+                    this.memory.pc++
+                }
+                break;
+            case OPCODES.IFE:
+                if (this.get(b) === this.get(a)) {
+                    this.memory.pc++
+                }
+                break;
+            case OPCODES.IFN:
+                if (this.get(b) !== this.get(a)) {
+                    this.memory.pc++
+                }
+                break;
+            case OPCODES.IFG:
+                if (this.get(b) > this.get(a)) {
+                    this.memory.pc++
+                }
+                break;
+            case OPCODES.IFL:
+                if (this.get(b) < this.get(a)) {
+                    this.memory.pc++
+                }
+                break;
+            case OPCODES.ADX:
+                this.set(b, this.get(b) + this.get(a) + this.memory.ex)
+                if (this.get(b) > 0xffff) {
+                    this.memory.ex = 0x0001
+                }
+            case OPCODES.SBX:
+                this.set(b, this.get(b) - this.get(a) + this.memory.ex)
+                if (this.get(b) < 0x0000) {
+                    this.memory.ex = 0xffff
+                }
+                break;
+            case OPCODES.STI:
+                this.set(b, this.get(a))
+                this.registers.I++
+                this.registers.J++
+                break;
+            case OPCODES.STD:
+                this.set(b, this.get(a))
+                this.registers.I--
+                this.registers.J--
+                break;
             default:
                 break;
         }
     }
 
-    countCycle(times = 1) {
+    set(key, value) {
+        if (key <= 0x07) {
+            this.registers[key] = value
+        }
+        else if (key & 0x10000) { return }
+        else {
+            this.memory[key] = value
+        }
+    }
 
+    get(key) {
+        if (key <= 0x07) {
+            return this.registers[key]
+        }
+        else if (key & 0x10000) { return }
+        else {
+            return this.memory[key]
+        }
+    }
+
+    countCycle(times = 1) {
+        for (var i = 0; i < times; i++) {
+            this.cycle()
+        }
     }
 
     getSpecialInstruction(binary) {
         return {
             opcode: (binary & 0x3ff),
             a: this.addressFor({ value: (binary >> 10) & 0x3f, isA: true })
-            //TODO addressfor
         }
     }
 
@@ -65,50 +188,55 @@ class Emulator {
             opcode: (binary & 0x1f),
             a: this.addressFor({ value: (binary >> 10) & 0x3f, isA: true }),
             b: this.addressFor({ value: (binary >> 5) & 0x1f, isA: false })
-            // TODO addressfor
         }
     }
 
-    addressFor({ value,isA}) {
-        var register, address
-        if( value <= 0x7){
+    addressFor({ value, isA }) {
+        var register, value
+        if (value <= 0x7) {
             register = this.registers[value]
         }
-        else if ( value <= 0x0f){
-            address = this.memory[value % 0x08]
+        else if (value <= 0x0f) {
+            value = this.memory[value % 0x08]
         }
-        else if ( value <= 0x17){
+        else if (value <= 0x17) {
             var nextInstruction = this.memory[this.memory.pc++]
-            address = (nextInstruction + this.memory[register])
+            value = this.memory[nextInstruction + this.registers[value % 0x08]]
         }
-        else if ( value === 0x19) {
-            var sp = this.memory.sp
-            return this.memory[sp]
-        } 
-        else if ( value === 0x1a) {
+        else if (value === 0x18) {
+            if (isA) {
+                return this.memory[this.memory.sp++]
+            }
+            else {
+                return this.memory[--this.memory.sp]
+            }
+        }
+        else if (value === 0x19) {
+            return this.memory[this.memory.sp]
+        }
+        else if (value === 0x1a) {
             var nextInstruction = this.memory[this.memory.pc++]
             var sp = this.memory.sp
-            return address = (nextInstruction + this.memory[nextInstruction + sp])
-        } 
-        else if ( value === 0x1b) {
+            return value = this.memory[nextInstruction + sp]
+        }
+        else if (value === 0x1b) {
             return this.memory.sp
         }
-        else if ( value === 0x1c) {
+        else if (value === 0x1c) {
             return this.memory.pc
         }
-        else if ( value === 0x1d ) {
+        else if (value === 0x1d) {
             return this.memory.ex
         }
-        else if ( value === 0x1e) {
+        else if (value === 0x1e) {
             return this.memory[this.memory.pc++]
         }
-        else if ( value === 0x1f) {
-            //TODO
+        else if (value === 0x1f) {
+            return this.memory[this.memory.pc++]
         }
-        else if ( value === 0x18) {
-            //TODO
+        else if (value >= 0x20 && value <= 0x3f) {
+            return value - 0x21
         }
-        
     }
 
 

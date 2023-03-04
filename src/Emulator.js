6f982be6e43f54aa4b29adcdce7cb9b68e838f83
BuildTools <unconfigured@null.spigotmc.org>
2023-03-04 12:22:23 -0300

    Add interruptions operations

--- src/Emulator.js
+++ src/Emulator.js
@@ -7,11 +7,18 @@ class Emulator {
         this.memory = []
         this.memory.length = this.ramSize
         this.registers = ["A", "B", "C", "X", "Y", "Z", "I", "J"]
+        this.cycle = 0
+        this.sp = 0xffff
+        this.pc = 0x0000
+        this.ex = 0x0000
+        this.ia = 0x0000
+        this.interruptQueue = []
+        this.triggerInterrupt = true
     }
 
     run() {
         for (var i = 0; i < Infinity; i++) {
-            countCycle()
+            this.cycle++
             var instruction = this.fetchInstruction()
             var decodedInstruction = this.decode(instruction)
             this.execute(decodedInstruction)
@@ -38,115 +45,188 @@ class Emulator {
         var a = decodedBinary.a
         var b = decodedBinary.b
 
-        switch (instruction.opcode) {
-            case OPCODES.SET:
-                this.set(b, this.get(a))
-            case OPCODES.ADD:
-                this.set(b, this.get(b) + this.get(a))
-                if (this.get(b) > 0xffff) {
-                    this.memory.ex = 0x0001
-                }
-                break;
-            case OPCODES.SUB:
-                this.set(b, this.get(b) - this.get(a))
-                if (this.get(b) < 0x0000) {
-                    this.memory.ex = 0xffff
-                }
-                break;
-            case OPCODES.MUL:
-                this.set(b, this.get(b) * this.get(a))
-                this.memory.ex = (this.get(b) >> 16) & 0xffff
-                break;
-            case OPCODES.DIV:
-                if (this.get(a) === 0) {
-                    this.set(b, 0)
-                    this.memory.ex = 0
-                } else {
-                    this.set(b, this.get(b) / this.get(a))
-                    this.memory.ex = ((this.get(b) << 16) / this.get(a)) & 0xffff
-                }
-                break;
-            case OPCODES.MOD:
-                if (this.get(a) === 0) {
-                    this.set(b, 0)
-                } else {
-                    this.set(b, this.get(b) % this.get(a))
-                }
-                break;
-            case OPCODES.AND:
-                this.set(b, this.get(b) & this.get(a))
-                break;
-            case OPCODES.BOR:
-                this.set(b, this.get(b) | this.get(a))
-                break;
-            case OPCODES.XOR:
-                this.set(b, this.get(b) ^ this.get(a))
-                break;
-            case OPCODES.SHR:
-                this.memory.ex = ((this.get(b) << 16) >> this.get(a)) & 0xffff
-                this.set(b, this.get(b) >>> this.get(a))
-                break;
-            case OPCODES.ASR:
-                this.memory.ex = ((this.get(b) << 16) >>> this.get(a)) & 0xffff
-                this.set(b, this.get(b) >> this.get(a))
-                break;
-            case OPCODES.SHL:
-                this.memory.ex = ((this.get(b) << this.get(a)) >> 16) & 0xffff
-                this.set(b, this.get(b) << this.get(a))
-                break;
-            case OPCODES.IFB:
-                if ((this.get(b) & this.get(a)) !== 0) {
-                    this.memory.pc++
-                }
-                break;
-            case OPCODES.IFC:
-                if ((this.get(b) & this.get(a)) === 0) {
-                    this.memory.pc++
-                }
-                break;
-            case OPCODES.IFE:
-                if (this.get(b) === this.get(a)) {
-                    this.memory.pc++
-                }
-                break;
-            case OPCODES.IFN:
-                if (this.get(b) !== this.get(a)) {
-                    this.memory.pc++
-                }
-                break;
-            case OPCODES.IFG:
-                if (this.get(b) > this.get(a)) {
-                    this.memory.pc++
-                }
-                break;
-            case OPCODES.IFL:
-                if (this.get(b) < this.get(a)) {
-                    this.memory.pc++
-                }
-                break;
-            case OPCODES.ADX:
-                this.set(b, this.get(b) + this.get(a) + this.memory.ex)
-                if (this.get(b) > 0xffff) {
-                    this.memory.ex = 0x0001
-                }
-            case OPCODES.SBX:
-                this.set(b, this.get(b) - this.get(a) + this.memory.ex)
-                if (this.get(b) < 0x0000) {
-                    this.memory.ex = 0xffff
-                }
-                break;
-            case OPCODES.STI:
-                this.set(b, this.get(a))
-                this.registers.I++
-                this.registers.J++
-                break;
-            case OPCODES.STD:
-                this.set(b, this.get(a))
-                this.registers.I--
-                this.registers.J--
-                break;
-            default:
-                break;
+        if (decodedBinary.opcode === 0x00) {
+            switch (instruction.opcode) {
+                case OPCODES.SET:
+                    this.cycle++
+                    this.set(b, this.get(a))
+                    break;
+                case OPCODES.ADD:
+                    this.cycle += 2
+                    this.set(b, this.get(b) + this.get(a))
+                    if (this.get(b) > 0xffff) {
+                        this.ex = 0x0001
+                    }
+                    break;
+                case OPCODES.SUB:
+                    this.cycle += 2
+                    this.set(b, this.get(b) - this.get(a))
+                    if (this.get(b) < 0x0000) {
+                        this.ex = 0xffff
+                    }
+                    break;
+                case OPCODES.MUL:
+                    this.cycle += 2
+                    this.set(b, this.get(b) * this.get(a))
+                    this.ex = (this.get(b) >> 16) & 0xffff
+                    break;
+                case OPCODES.DIV:
+                    this.cycle += 3
+                    if (this.get(a) === 0) {
+                        this.set(b, 0)
+                        this.ex = 0
+                    } else {
+                        this.set(b, this.get(b) / this.get(a))
+                        this.ex = ((this.get(b) << 16) / this.get(a)) & 0xffff
+                    }
+                    break;
+                case OPCODES.MOD:
+                    this.cycle += 3
+                    if (this.get(a) === 0) {
+                        this.set(b, 0)
+                    } else {
+                        this.set(b, this.get(b) % this.get(a))
+                    }
+                    break;
+                case OPCODES.AND:
+                    this.cycle++
+                    this.set(b, this.get(b) & this.get(a))
+                    break;
+                case OPCODES.BOR:
+                    this.cycle++
+                    this.set(b, this.get(b) | this.get(a))
+                    break;
+                case OPCODES.XOR:
+                    this.cycle++
+                    this.set(b, this.get(b) ^ this.get(a))
+                    break;
+                case OPCODES.SHR:
+                    this.cycle++
+                    this.ex = ((this.get(b) << 16) >> this.get(a)) & 0xffff
+                    this.set(b, this.get(b) >>> this.get(a))
+                    break;
+                case OPCODES.ASR:
+                    this.cycle++
+                    this.ex = ((this.get(b) << 16) >>> this.get(a)) & 0xffff
+                    this.set(b, this.get(b) >> this.get(a))
+                    break;
+                case OPCODES.SHL:
+                    this.cycle++
+                    this.ex = ((this.get(b) << this.get(a)) >> 16) & 0xffff
+                    this.set(b, this.get(b) << this.get(a))
+                    break;
+                case OPCODES.IFB:
+                    this.cycle += 2
+                    if ((this.get(b) & this.get(a)) !== 0) {
+                        this.pc++
+                    }
+                    break;
+                case OPCODES.IFC:
+                    this.cycle += 2
+                    if ((this.get(b) & this.get(a)) === 0) {
+                        this.pc++
+                    }
+                    break;
+                case OPCODES.IFE:
+                    this.cycle += 2
+                    if (this.get(b) === this.get(a)) {
+                        this.pc++
+                    }
+                    break;
+                case OPCODES.IFN:
+                    this.cycle += 2
+                    if (this.get(b) !== this.get(a)) {
+                        this.pc++
+                    }
+                    break;
+                case OPCODES.IFG:
+                    this.cycle += 2
+                    if (this.get(b) > this.get(a)) {
+                        this.pc++
+                    }
+                    break;
+                case OPCODES.IFL:
+                    this.cycle += 2
+                    if (this.get(b) < this.get(a)) {
+                        this.pc++
+                    }
+                    break;
+                case OPCODES.ADX:
+                    this.cycle += 3
+                    this.set(b, this.get(b) + this.get(a) + this.ex)
+                    if (this.get(b) > 0xffff) {
+                        this.ex = 0x0001
+                    }
+                case OPCODES.SBX:
+                    this.cycle += 3
+                    this.set(b, this.get(b) - this.get(a) + this.ex)
+                    if (this.get(b) < 0x0000) {
+                        this.ex = 0xffff
+                    }
+                    break;
+                case OPCODES.STI:
+                    this.cycle += 2
+                    this.set(b, this.get(a))
+                    this.registers.I++
+                    this.registers.J++
+                    break;
+                case OPCODES.STD:
+                    this.cycle += 2
+                    this.set(b, this.get(a))
+                    this.registers.I--
+                    this.registers.J--
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            switch (decodedBinary.opcode) {
+                case OPCODES.JSR:
+                    this.cycle += 3
+                    this.sp = (this.sp - 1) & 0xffff
+                    this.set(this.sp, this.pc + 1)
+                    this.pc = this.get(decodedBinary.a)
+                    break
+                case OPCODES.INT:
+                    this.cycle += 4
+                    if (!this.triggerInterrupt) {
+                        this.interruptQueue.push(this.get(a))
+                    } else {
+                        if (this.ia !== 0) {
+                            this.sp = (this.sp - 1) & 0xffff
+                            this.pc = this.ia
+                            this.sp = (this.sp - 1) & 0xffff
+                            this.set(0x00, this.get(a))
+                        }
+                    }
+                    break
+                case OPCODES.IAG:
+                    this.cycle++
+                    this.set(a, this.ia)
+                    break
+                case OPCODES.IAS:
+                    this.cycle++
+                    this.ia = this.get(a)
+                    break
+                case OPCODES.RFI:
+                    this.cycle += 3
+                    this.triggerInterrupt = true
+                    this.sp = (this.sp + 1) & 0xffff
+                    this.set(a, this.sp)
+                    this.sp = (this.sp + 1) & 0xffff
+                    this.set(pc, this.sp)
+                    break
+                case OPCODES.IAQ:
+                    this.cycle++
+                    if (this.get(a)) {
+                        this.triggerInterrupt = false
+                    } else {
+                        this.triggerInterrupt = true
+                    }
+                    break
+                
+                // devices operations
+            }
         }
     }
 
@@ -167,12 +247,6 @@ class Emulator {
         else if (key & 0x10000) { return }
         else {
             return this.memory[key]
-        }
-    }
-
-    countCycle(times = 1) {
-        for (var i = 0; i < times; i++) {
-            this.cycle()
         }
     }
 
@@ -200,39 +274,39 @@ class Emulator {
             value = this.memory[value % 0x08]
         }
         else if (value <= 0x17) {
-            var nextInstruction = this.memory[this.memory.pc++]
+            var nextInstruction = this.memory[this.pc++]
             value = this.memory[nextInstruction + this.registers[value % 0x08]]
         }
         else if (value === 0x18) {
             if (isA) {
-                return this.memory[this.memory.sp++]
+                return this.memory[this.sp++]
             }
             else {
-                return this.memory[--this.memory.sp]
+                return this.memory[--this.sp]
             }
         }
         else if (value === 0x19) {
-            return this.memory[this.memory.sp]
+            return this.memory[this.sp]
         }
         else if (value === 0x1a) {
-            var nextInstruction = this.memory[this.memory.pc++]
-            var sp = this.memory.sp
+            var nextInstruction = this.memory[this.pc++]
+            var sp = this.sp
             return value = this.memory[nextInstruction + sp]
         }
         else if (value === 0x1b) {
-            return this.memory.sp
+            return this.sp
         }
         else if (value === 0x1c) {
-            return this.memory.pc
+            return this.pc
         }
         else if (value === 0x1d) {
-            return this.memory.ex
+            return this.ex
         }
         else if (value === 0x1e) {
-            return this.memory[this.memory.pc++]
+            return this.memory[this.pc++]
         }
         else if (value === 0x1f) {
-            return this.memory[this.memory.pc++]
+            return this.memory[this.pc++]
         }
         else if (value >= 0x20 && value <= 0x3f) {
             return value - 0x21
